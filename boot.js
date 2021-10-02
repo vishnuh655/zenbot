683c11a93b1ccbeb57e505689390ceff72fed366
vishnuh655 <vishnuh655@gmail.com>
2021-10-02 20:13:08 +0530

    DB Changes

--- boot.js
+++ boot.js
@@ -3,6 +3,7 @@ var path = require("path");
 var minimist = require("minimist");
 var version = require("./package.json").version;
 var EventEmitter = require("events");
+var Datastore = require("@seald-io/nedb");
 
 module.exports = function (cb) {
   var zenbot = { version };
@@ -37,86 +38,37 @@ module.exports = function (cb) {
   var eventBus = new EventEmitter();
   zenbot.conf.eventBus = eventBus;
 
-  var authStr = "",
-    authMechanism,
-    connectionString;
-
-  if (zenbot.conf.mongo.username) {
-    authStr = encodeURIComponent(zenbot.conf.mongo.username);
-
-    if (zenbot.conf.mongo.password)
-      authStr += ":" + encodeURIComponent(zenbot.conf.mongo.password);
-
-    authStr += "@";
-
-    // authMechanism could be a conf.js parameter to support more mongodb authentication methods
-    authMechanism = zenbot.conf.mongo.authMechanism || "DEFAULT";
-  }
-
-  if (zenbot.conf.mongo.connectionString) {
-    connectionString = zenbot.conf.mongo.connectionString;
-  } else {
-    connectionString =
-      "mongodb://" +
-      authStr +
-      zenbot.conf.mongo.host +
-      ":" +
-      zenbot.conf.mongo.port +
-      "/" +
-      zenbot.conf.mongo.db +
-      "?" +
-      (zenbot.conf.mongo.replicaSet
-        ? "&replicaSet=" + zenbot.conf.mongo.replicaSet
-        : "") +
-      (authMechanism ? "&authMechanism=" + authMechanism : "");
-  }
-
-  const Datastore = require("@seald-io/nedb");
+  //4. Setup Datastore
   const db = {};
-  db.trades = new Datastore({ filename: "data/trades", autoload: true });
-  db.resume_markers = new Datastore({
-    filename: "data/resume_markers",
+  const dataPath = "data/";
+  db.trades = new Datastore({
+    filename: dataPath + "trades",
     autoload: true,
   });
-  db.balances = new Datastore({ filename: "data/balances" });
-  db.sessions = new Datastore({ filename: "data/sessions" });
-  db.periods = new Datastore({ filename: "data/periods" });
-  db.my_trades = new Datastore({ filename: "data/my_trades" });
-  db.sim_results = new Datastore({ filename: "data/sim_results" });
+  db.resume_markers = new Datastore({
+    filename: dataPath + "resume_markers",
+    autoload: true,
+  });
+  db.balances = new Datastore({
+    filename: dataPath + "balances",
+    autoload: true,
+  });
+  db.sessions = new Datastore({
+    filename: dataPath + "sessions",
+    autoload: true,
+  });
+  db.periods = new Datastore({
+    filename: dataPath + "periods",
+    autoload: true,
+  });
+  db.my_trades = new Datastore({
+    filename: dataPath + "my_trades",
+    autoload: true,
+  });
+  db.sim_results = new Datastore({
+    filename: dataPath + "sim_results",
+    autoload: true,
+  });
   _.set(zenbot, "conf.db", db);
   cb(null, zenbot);
-
-  // db.loadDatabase(function (err) {
-  //   if (err) {
-  //     console.error("WARNING: MongoDB Connection Error: ", err);
-  //     console.error(
-  //       "WARNING: without MongoDB some features (such as backfilling/simulation) may be disabled."
-  //     );
-  //     console.error("Attempted authentication string: " + connectionString);
-  //     cb(null, zenbot);
-  //     return;
-  //   }
-  //   _.set(zenbot, "conf.db.mongo", db);
-  //   cb(null, zenbot);
-  //   // Now commands will be executed
-  // });
-
-  // require("mongodb").MongoClient.connect(
-  //   connectionString,
-  //   { useNewUrlParser: true, useUnifiedTopology: true },
-  //   function (err, client) {
-  //     if (err) {
-  //       console.error("WARNING: MongoDB Connection Error: ", err);
-  //       console.error(
-  //         "WARNING: without MongoDB some features (such as backfilling/simulation) may be disabled."
-  //       );
-  //       console.error("Attempted authentication string: " + connectionString);
-  //       cb(null, zenbot);
-  //       return;
-  //     }
-  //     var db = client.db(zenbot.conf.mongo.db);
-  //     _.set(zenbot, "conf.db.mongo", db);
-  //     cb(null, zenbot);
-  //   }
-  // );
 };
